6385beabe89644d97d94a5c7a1a161b10eaf6dd7
matthew elia <papyrustigris@matthews-Mini.cable.rcn.com>
2015-01-26 02:11:28 -0500

    added fontawesome + (ref) data

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -15,7 +15,8 @@ angular
     'ngResource',
     'ngSanitize',
     'ngTouch',
-    'ui.router'
+    'ui.router',
+    'firebase'
   ])
   
   .config(function ($stateProvider, $urlRouterProvider) {
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,3 +1,5 @@
+/* global Firebase */
+
 'use strict';
 
 /**
@@ -8,10 +10,43 @@
  * Controller of the tigrisApp
  */
 angular.module('tigrisApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('MainCtrl', function ($scope, $firebase) {
+    var ref = new Firebase('https://papertigers.firebaseio.com/users');
+	var sync = $firebase(ref);
+
+	$scope.doLoginFb = function () {
+		ref.authWithOAuthPopup('facebook', function (error, authData) {
+			if (error) {
+				console.log('login failed!', error);
+			} else {
+				sync.$push(authData).then(function () {
+					console.log('authenticated successfully with payload:', authData);	
+				});
+			}
+		});
+	};
+
+	$scope.doLoginGoogle = function () {
+		ref.authWithOAuthPopup('google', function (error, authData) {
+			if (error) {
+				console.log('login failed!', error);
+			} else {
+				sync.$push(authData).then(function () {
+					console.log('authenticated successfully with payload:', authData);	
+				});
+			}
+		});
+	};
+
+	$scope.doLoginTwitter = function () {
+		ref.authWithOAuthPopup('twitter', function (error, authData) {
+			if (error) {
+				console.log('login failed!', error);
+			} else {
+				sync.$push(authData).then(function () {
+					console.log('authenticated successfully with payload:', authData);	
+				});
+			}
+		});
+	};
   });
